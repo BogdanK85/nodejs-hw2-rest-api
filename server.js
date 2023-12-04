382fa599f6cc53aed16229319d3fd6cb6a349836
BogdanK85 <126570087+BogdanK85@users.noreply.github.com>
2023-12-04 21:20:17 +0200

    q

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const DB_HOST =
   "mongodb://Bogdan:5BT4i6q594KFGCUr@ac-xiacy5r-shard-00-00.xd8pay8.mongodb.net:27017,ac-xiacy5r-shard-00-01.xd8pay8.mongodb.net:27017,ac-xiacy5r-shard-00-02.xd8pay8.mongodb.net:27017/?ssl=true&replicaSet=atlas-lnmci9-shard-0&authSource=admin&retryWrites=true&w=majority";
 // "mongodb://kobakbogdan:SOI9B9Ne8rYyoui5@ac-xzacd0x-shard-00-00.ok1ifvm.mongodb.net:27017,ac-xzacd0x-shard-00-01.ok1ifvm.mongodb.net:27017,ac-xzacd0x-shard-00-02.ok1ifvm.mongodb.net:27017/hwDBtest?ssl=true&replicaSet=atlas-rg0nuj-shard-0&authSource=admin&retryWrites=true&w=majority";
 // "mongodb://kobakbogdan:SOI9B9Ne8rYyoui5@ac-xzacd0x-shard-00-00.ok1ifvm.mongodb.net:27017,ac-xzacd0x-shard-00-01.ok1ifvm.mongodb.net:27017,ac-xzacd0x-shard-00-02.ok1ifvm.mongodb.net:27017/homework-1?ssl=true&replicaSet=atlas-rg0nuj-shard-0&authSource=admin&retryWrites=true&w=majority";
-// "mongodb+srv://borys:ba8DfeBGRbBMv2c7@cluster0.ndb6m.mongodb.net/goit-86";
 // "mongodb+srv://kobakbogdan:SOI9B9Ne8rYyoui5@homework.ok1ifvm.mongodb.net/hw-03?retryWrites=true&w=majority";
 // "mongodb+srv://Bogdan:5BT4i6q594KFGCUr@cluster0.xd8pay8.mongodb.net/db-contacts?retryWrites=true&w=majority";
 
