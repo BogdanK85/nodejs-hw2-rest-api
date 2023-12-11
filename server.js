b0f0d23564360a16d91f2b4331c3d5fa9685cc1f
BogdanK85 <126570087+BogdanK85@users.noreply.github.com>
2023-12-11 14:07:30 +0200

    add auth route

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const mongoose = require("mongoose");
 
 const app = require("./app");
 
-const { DB_HOST } = process.env;
+// const { DB_HOST } = process.env;
+const DB_HOST =
+  "mongodb://Bogdan:5BT4i6q594KFGCUr@ac-xiacy5r-shard-00-00.xd8pay8.mongodb.net:27017,ac-xiacy5r-shard-00-01.xd8pay8.mongodb.net:27017,ac-xiacy5r-shard-00-02.xd8pay8.mongodb.net:27017/db-contacts?ssl=true&replicaSet=atlas-lnmci9-shard-0&authSource=admin&retryWrites=true&w=majority";
 
-mongoose.set("strictQuery", true);
+// mongoose.set("strictQuery", true);
 
 mongoose
   .connect(DB_HOST)
